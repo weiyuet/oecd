ce583600137df74bc2354b68fc43bb4ffa6c084f
weiyuet <weiyuet@gmail.com>
2022-10-18 18:46:45 +0800

    Edited plot of crude oil production

--- code/crude-oil-production.R
+++ code/crude-oil-production.R
@@ -10,8 +10,8 @@ crude_oil_production_annual <- read_csv("data/crude_oil_production_annual.csv")
 crude_oil_production_annual %>%
   distinct(crude_oil_production_annual$LOCATION) %>% count()
 
-# Plot Crude oil production in Russia, compared to Ukraine, Saudi Arabia, Norway, the USA, and the EU
-selected_countries <- c("RUS", "UKR", "SAU", "NOR", "USA", "EU28")
+# Plot Crude oil production in Russia, compared to Ukraine, Saudi Arabia, Norway, the USA, China, and the EU
+selected_countries <- c("RUS", "UKR", "SAU", "NOR", "USA", "CHN", "EU28")
 
 crude_oil_production_annual %>% 
   filter(LOCATION %in% selected_countries) %>%
@@ -22,17 +22,18 @@ crude_oil_production_annual %>%
                      limits = c(1970, 2020),
                      expand = c(0.01, 0)) +
   scale_y_log10(labels = label_number(suffix = " toe", big.mark = ","),
-                limits = c(1000, 1000000),
-                expand = c(0, 0)) +
+                limits = c(100, 1000000),
+                expand = c(0.01, 0)) +
+  scale_colour_manual(values = natparks.pals("Yellowstone", 7)) +
   theme_bw() +
-  theme(legend.position = c(0.9, 0.35), 
-        legend.title = element_blank(),
+  theme(legend.position = c(0.3, 0.3), 
+        legend.background = element_blank(),
         axis.text.y = element_text(angle = 90)) +
-  scale_colour_manual(values = natparks.pals("Yellowstone", 6)) +
-  labs(x = "", y = "",
+  labs(x = "", y = "log scale",
+       colour = "",
        title = "Crude Oil Production (Total Annual)",
-       subtitle = "Measured in thousand tonne of oil equivalent (toe) | y-axis log scale",
+       subtitle = "Measured in thousand tonne of oil equivalent (toe)",
        caption = "Data: OECD (2022), Crude oil production (indicator). doi: 10.1787/4747b431-en | Graphic: @weiyuet")
   
 # Save image
-ggsave("figures/russia-crude-oil-production.png", width = 8, height = 6)
+ggsave("figures/crude-oil-production.png", width = 8, height = 6)
