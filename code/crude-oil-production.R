ddbb560e50d23823c9947ba5829f50d17f409ce7
weiyuet <weiyuet@gmail.com>
2022-10-20 18:16:54 +0800

    Edited plot of crude oil production

--- code/crude-oil-production.R
+++ code/crude-oil-production.R
@@ -20,19 +20,17 @@ crude_oil_production_annual %>%
   geom_line() +
   geom_point(size = 0.7) +
   scale_x_continuous(breaks = seq(1970, 2020, 5),
-                     limits = c(1970, 2020)) +
+                     limits = c(1970, 2017)) +
   scale_y_log10(labels = label_number(suffix = " ktoe", big.mark = ","),
                 limits = c(100, 1000000)) +
   scale_colour_jco() +
   theme_bw() +
-  theme(legend.position = c(0.5, 0.1), 
+  theme(legend.position = "right", 
         legend.background = element_blank(),
         axis.text.y = element_text(angle = 90)) +
-  guides(colour = guide_legend(nrow = 1)) +
   labs(x = "", y = "",
        colour = "",
        title = "Crude Oil Production (Total Annual)",
-       subtitle = "Measured in thousand tonne of oil equivalent (ktoe)",
        caption = "Data: OECD (2022), Crude oil production (indicator). doi: 10.1787/4747b431-en | Graphic: @weiyuet")
   
 # Save image
