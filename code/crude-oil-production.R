fbf17d19c568426b85b6963f983ea2af4d1136fd
weiyuet <weiyuet@gmail.com>
2023-03-25 18:46:26 +0800

    Edited code and updated plot

--- code/crude-oil-production.R
+++ code/crude-oil-production.R
@@ -9,13 +9,15 @@ library(scales)
 library(paletteer)
 
 #### Load data ####
-crude_oil_production_annual <- read_csv("data/crude_oil_production_annual.csv")
+crude_oil_production_annual <- read_csv("data/crude-oil-production.csv")
 
-#### Column names to lower case ####
+#### Wrangle ####
+# Column names to lower case
 crude_oil_production_annual <- crude_oil_production_annual %>%
   clean_names()
 
-#### Plot Crude oil production ####
+#### Visualize ####
+# Plot Crude oil production
 selected_locations <- c("EU28", "G20", "BRN", "CHN", "NOR", "RUS", "SAU", "USA", "VEN")
 
 crude_oil_production_annual %>% 
@@ -25,20 +27,21 @@ crude_oil_production_annual %>%
              y = value,
              colour = location)) +
   geom_line() +
+  geom_point(size = 0.5) +
   scale_x_continuous(breaks = seq(1960, 2020, 5),
                      limits = c(1970, 2017)) +
   scale_y_log10(labels = label_number(big.mark = "",
                                       accuracy = 0.1)) +
   scale_colour_paletteer_d("ggsci::default_jco") +
-  theme_bw() +
-  theme(legend.position = "right", 
-        legend.background = element_blank()) +
   labs(x = "",
        y = "",
        colour = "",
        title = "Crude Oil Production (Total Annual)",
        subtitle = "million toe (y-axis log scale)",
-       caption = "Data: OECD (2022), Crude oil production (indicator). doi: 10.1787/4747b431-en | Graphic: @weiyuet")
+       caption = "Data: OECD (2022), Crude oil production (indicator). doi: 10.1787/4747b431-en | Graphic: @weiyuet") +
+  theme_bw() +
+  theme(legend.position = "right", 
+        legend.background = element_blank())
   
 #### Save image ####
 ggsave("figures/crude-oil-production.png", width = 7, height = 7)
