c1a6a0fb71d60f891820aed614f2b700b5a9b1ed
weiyuet <weiyuet@gmail.com>
2023-03-25 16:28:02 +0800

    Edited code, and updated plots

--- code/nuclear-power-plants.R
+++ code/nuclear-power-plants.R
@@ -1,32 +1,38 @@
-#########################
-# Nuclear Power Plants  #
-#########################
+########################
+# Nuclear Power Plants #
+########################
 
 #### Setup ####
 library(tidyverse)
 library(janitor)
 library(scales)
+library(glue)
 
 #### Load data ####
 nuclear_plants <- read_csv("data/nuclear-plants.csv")
 
-#### Column names to lower case ####
+#### Wrangle ####
+# Column names to lower case
 nuclear_plants <- nuclear_plants %>% 
   clean_names()
 
-#### Plot number of nuclear power plants ####
+#### Visualize ####
+# Plot number of nuclear power plants
 nuclear_plants %>% 
   filter(location != "OECD" & location != "OECDE") %>% 
-  mutate(location = fct_reorder(location, value)) %>% 
-  ggplot(aes(x = value, y = location)) +
+  mutate(location = fct_reorder(location, value)) %>%
+  ggplot(aes(x = value,
+             y = location)) +
   geom_col() +
   scale_x_continuous(expand = c(0.01, 0),
                      limits = c(0, 100),
                      breaks = seq(0, 100, 10)) +
-  theme_bw() +
-  labs(x = "", y = "",
+  labs(x = "",
+       y = "",
        title = "Number of Nuclear Power Plants",
-       caption = "Data: OECD (2022), Nuclear power plants (indicator). doi: 10.1787/3cc1191d-en | Graphic: @weiyuet")
+       subtitle = glue("Year of data = {max(nuclear_plants$time)}"),
+       caption = "Data: OECD (2022), Nuclear power plants (indicator). doi: 10.1787/3cc1191d-en | Graphic: @weiyuet") +
+  theme_bw()
 
 #### Save image ####
 ggsave("figures/nuclear-power-plants.png", width = 6, height = 6)
--- code/renewable-energy.R
+++ code/renewable-energy.R
@@ -1,55 +1,68 @@
-# Setup
+####################
+# Renewable Energy #
+####################
+
+#### Setup ####
 library(tidyverse)
 library(janitor)
 library(scales)
 library(paletteer)
 
-# Load data
+#### Load data ####
 renewable_energy <- read_csv("data/renewable-energy.csv")
 
+#### Wrangle ####
 # Column names to lower case
 renewable_energy <- renewable_energy %>% 
   clean_names()
 
+#### Visualize ####
 # What is the gross production of renewable energy?
-selected_locations <- c("WLD", "AUS", "BRA", "CHN", "DEU", "IDN", "ISL", "JPN", "USA")
+selected_locations <- c("WLD", "AUS", "BRA", "CHN", "DEU", "FRA", "IDN", "ISL", "JPN", "USA")
 
 renewable_energy %>% 
   filter(location %in% selected_locations) %>% 
   filter(measure == "KTOE") %>%
   mutate(value = value / 1000) %>% 
-  ggplot(aes(x = time, y = value, colour = location)) +
+  ggplot(aes(x = time,
+             y = value,
+             colour = location)) +
   geom_step() +
   scale_x_continuous(breaks = seq(1960, 2020, 5)) +
   scale_y_log10(labels = label_number(big.mark = "")) +
   scale_colour_paletteer_d("ggsci::default_jco") +
-  theme_bw() +
-  labs(x = "", y = "",
+  labs(x = "",
+       y = "",
        colour = "",
        title = "Renewable Energy Production",
        subtitle = "million toe (y-axis log scale)",
-       caption = "Data: OECD (2022), Renewable energy (indicator). doi: 10.1787/aac7c3f1-en | Graphic: @weiyuet")
+       caption = "Data: OECD (2022), Renewable energy (indicator). doi: 10.1787/aac7c3f1-en | Graphic: @weiyuet") +
+  theme_bw()
 
-# Save image
+#### Save image ####
 ggsave("figures/renewable-energy.png", width = 7, height = 7)
 
+#### Visualize ####
 # What is the percentage of renewable energy per primary energy supply?
 selected_locations <- c("ISL", "JPN", "NOR")
 
 renewable_energy %>% 
   filter(location %in% selected_locations) %>% 
   filter(measure == "PC_PRYENRGSUPPLY") %>% 
-  ggplot(aes(x = time, y = value, colour = location)) +
+  ggplot(aes(x = time,
+             y = value,
+             colour = location)) +
   geom_step() +
   scale_x_continuous(breaks = seq(1990, 2020, 5)) +
   scale_y_continuous(breaks = seq(0, 100, 10),
                      labels = label_number(suffix = "%")) +
   scale_color_paletteer_d("ggsci::default_jco") +
-  theme_bw() +
-  labs(x = "", y = "",
+  labs(x = "",
+       y = "",
        colour = "",
        title = "Pecentage of Primary Energy Supply from Renewable Energy",
-       caption = "Data: OECD (2022), Renewable energy (indicator). doi: 10.1787/aac7c3f1-en | Graphic: @weiyuet")
+       caption = "Data: OECD (2022), Renewable energy (indicator). doi: 10.1787/aac7c3f1-en | Graphic: @weiyuet") +
+  theme_bw()
 
-# Save image
+#### Save image ####
 ggsave("figures/percentage-renewable-energy.png", width = 7, height = 7)
