0dbfc43970e53e7174e4170d1ba531d4f549e453
weiyuet <weiyuet@gmail.com>
2023-04-14 18:45:45 +0800

    Edited code and plot

--- code/road-accidents.R
+++ code/road-accidents.R
@@ -5,6 +5,7 @@
 #### Setup ####
 library(tidyverse)
 library(janitor)
+library(skimr)
 library(scales)
 
 #### Load Data ####
@@ -15,6 +16,27 @@ road_accidents <- read_csv("data/road-accidents.csv")
 # Column names to lower case
 road_accidents <- road_accidents %>% 
   clean_names()
+
+#### Explore Data ####
+road_accidents %>% 
+  skim() %>% summary() # Over 9300 rows in the data set
+
+range(road_accidents$time) # Data from 1970 to 2021
+
+road_accidents %>% 
+  count(frequency, sort = TRUE) # All annual data
+
+road_accidents %>% 
+  count(measure, sort = TRUE) # NBR measure? It's the most numerous measure in the data set
+
+road_accidents %>% 
+  count(subject, sort = TRUE) # Includes deaths, injuries and casual accidents
+
+range(road_accidents$indicator) # Only road accidents
+
+road_accidents %>% 
+  count(location, sort = TRUE) %>% 
+  print(n = 60) # 60 countries in the data set
 
 #### Visualize ####
 # Are the number of road accidents increasing or decreasing?
@@ -34,7 +56,7 @@ road_accidents %>%
   scale_y_continuous(labels = label_number(big.mark = ",")) +
   labs(x = "",
        y = "",
-       title = "Number of Road Accidents",
+       title = "Number of Road Accidents Resulting in Death",
        subtitle = "Trending downwards for most regions in the world",
        caption = "Data: OECD (2023), Road accidents (indicator). doi: 10.1787/2fe1b899-en | Graphic: @weiyuet") +
   theme_bw() +
